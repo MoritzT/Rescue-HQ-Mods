d9fa6c77a2efe89765773c66dc2db3f1a48e0916
Moritz Tolxdorff <moritz.tolxdorff@gmail.com>
2019-06-16 09:17:16 -0400

    small changes to money and adding reputation

--- Money/Money.fsx
+++ Money/Money.fsx
@@ -5,10 +5,28 @@ for scenario in Def.Scenario |> Seq.toArray do
     Def.Scenario.Update scenario.ID (fun x -> 
         {x with 
             EconomySystem = {
-                InitialFunds = 999_999_999
-                AwardedCityGrants = 3
-                MoneyPerCityGrant = 10_000
+                    InitialFunds = 100_000_000
+                    AwardedCityGrants = 99
+                    MoneyPerCityGrant = 1_000_000
             }
+            ReputationSystem = {
+                MoneyPerReputation = 1000
+                Ranks = [
+                    {MaxReputation = 60000; CityHallPayments = 80_000}
+
+                ]
+            }
+            EndlessMode = x.EndlessMode @ [
+                {
+                    ID = "99"
+                    TimeLine = [
+                        TimeLine.At (gt 0 6 23.) [
+                            Hq.Reputation.Income 60000
+                            Hq.Money.Income 1000000 
+                            ]
+                    ]
+                }
+            ]
         }
     )
 
